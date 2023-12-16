5c71221f9509af8387626f0e09cd78872353e207
unknown <raptorshah1769@gmail.com>
2023-12-16 18:01:39 +0530

    Revert "Comming soon page"
    This reverts commit 132286847c8414938883873bcda875c93880970c.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <div className="bg_img">
-        <div className="header">VIBRANT SOLUTIONS</div>
-        <div className="filter-image">
-          <div className="coming-soon">WE'RE COMING SOON</div>
-        </div>
-      </div>
-      {/* </div> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
